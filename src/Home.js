735a04f04ca090cd31304ccb574bbf5cee95fcfe
Ghost620 <faghost6201@gmail.com>
2023-04-07 20:43:28 +0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -54,11 +54,11 @@ const Home = ({ navBar, setNavBar }) => {
   }, []);
 
   return (
-    <div className='mt-16 w-full h-screen flex items-center justify-center bg-[#e6f6fd] dark:bg-slate-900'>
+    <div className='mt-16 w-full h-fit lg:h-screen flex items-center justify-center bg-[#e6f6fd] dark:bg-slate-900'>
 
-      <div className='w-full lg:max-w-[90%] h-[90%] flex items-center space-x-6 p-6'>
+      <div className='w-full lg:max-w-[90%] h-[90%] flex flex-col lg:flex-row space-y-4 lg:space-y-0 items-center lg:space-x-6 p-6'>
 
-        <div className='w-1/2 h-fit shadow-2xl'>
+        <div className='w-full lg:w-1/2 h-fit shadow-2xl'>
           <div className='rounded-2xl overflow-hidden'>
             
             <video ref={videoRef} controls>
@@ -69,39 +69,39 @@ const Home = ({ navBar, setNavBar }) => {
         </div>
         
         {showImage &&
-        <div className='flex flex-col items-center justify-center space-y-4 w-1/2 h-full border-2 border-black dark:border-white rounded-2xl p-4'>
+        <div className='flex flex-col items-center justify-center space-y-4 w-full lg:w-1/2 h-full lg:border-2 border-black dark:border-white rounded-2xl lg:p-4'>
 
-          <div className='flex w-[90%]'>
+          <div className='flex flex-col lg:flex-row w-[90%] space-y-2 lg:space-y-0'>
             <motion.img
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ ease: 'linear', duration: 3 }}
               src='https://pbs.twimg.com/media/FHEkN7GWUA4Xyjt?format=jpg&name=small'
               alt=''
-              className='h-64 w-64 rounded-3xl shadow-2xl'
+              className='h-64 lg:w-64 w-full object-contain rounded-3xl shadow-2xl'
             />
 
-            <motion.div className='flex flex-col justify-center px-2 space-y-1' variants={container} ref={ref} initial={"hidden"} animate={controls}>
+            <motion.div className='flex flex-col items-center lg:items-start justify-center px-2 space-y-1 lg:space-x-0' variants={container} ref={ref} initial={"hidden"} animate={controls}>
 
-              <motion.div variants={item} className='text-4xl font-extrabold bg-black dark:bg-white w-fit p-2 pr-8 rounded-r-3xl'>
+              <motion.div variants={item} className='text-xl h-fit lg:text-4xl font-extrabold bg-black dark:bg-white text-center w-full lg:w-fit p-2 lg:pr-8 rounded-2xl lg:rounded-r-3xl'>
                 <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>
                   Shari Baloch
                 </span>
               </motion.div>
 
-              <motion.div variants={item} className='text-4xl font-extrabold bg-black dark:bg-white w-fit p-2 pr-8 rounded-r-3xl'>
+              <motion.div variants={item} className='text-xl h-fit lg:text-4xl font-extrabold bg-black dark:bg-white text-center w-full lg:w-fit p-2 lg:pr-8 rounded-2xl lg:rounded-r-3xl'>
                 <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>
                   Age: 30
                 </span>
               </motion.div>
 
-              <motion.div variants={item} className='text-4xl font-extrabold bg-black dark:bg-white w-fit p-2 pr-8 rounded-r-3xl'>
+              <motion.div variants={item} className='text-xl h-fit lg:text-4xl font-extrabold bg-black dark:bg-white text-center w-full lg:w-fit p-2 lg:pr-8 rounded-2xl lg:rounded-r-3xl'>
                 <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>
                   Female
                 </span>
               </motion.div>
 
-              <motion.div variants={item} className='text-2xl font-extrabold bg-black dark:bg-white w-[75%] p-2 rounded-r-3xl'>
+              <motion.div variants={item} className='text-xl h-fit lg:text-3xl font-extrabold bg-black dark:bg-white text-center lg:text-left w-full lg:w-fit p-2 lg:pr-8 rounded-2xl lg:rounded-r-3xl'>
                 <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>
                   Organization: Balochistan Liberation Army
                 </span>
@@ -110,7 +110,7 @@ const Home = ({ navBar, setNavBar }) => {
 
           </div>
           
-          <div className='text-justify text-xl w-[90%] bg-white dark:bg-slate-400 p-8 rounded-3xl drop-shadow-2xl'>
+          <div className='text-justify text-xl w-[90%] bg-white dark:bg-slate-400 p-8 rounded-3xl drop-shadow-2xl overflow-auto'>
             The bomber was identified as Shari Baloch, a 30-year-old female and secondary school science teacher from Kech District in Balochistan. She held bachelor's and master's degrees in education from the Allama Iqbal Open University, and had also obtained a master's degree in zoology from the University of Balochistan. She had reportedly enrolled in another postgraduate degree at the University of Karachi months before the attack, but was not reported to be a student there at the time.
           </div>
 
